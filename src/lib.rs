2c2724ee3dff382b527deae5c1b0627b2b09a997
mmagician <marcin.gorny.94@protonmail.com>
2023-08-23 22:23:48 +0200

    get rid of unnecessary modules

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,9 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 
-pub mod hash;
 pub mod poseidon;
 
 pub use halo2_base::halo2_proofs::halo2curves::bn256::Fr as Bn256Fr;
-pub use hash::{Hashable, HASHABLE_DOMAIN_SPEC};
 
 /// a default step can be compatible with codehash circuit
 pub const DEFAULT_STEP: usize = 62;
