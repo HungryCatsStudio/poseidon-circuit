586aa28f4220d2acbb982d8e962d45e3c8c24150
Antonio Mejías Gil <anmegi.95@gmail.com>
2023-08-29 11:57:52 +0200

    changed back to 5 elements in test

--- src/poseidon/pow5.rs
+++ src/poseidon/pow5.rs
@@ -851,12 +851,15 @@ mod tests {
         let message = [
             Fp::random(rng),
             Fp::random(rng),
+            Fp::random(rng),
+            Fp::random(rng),
+            Fp::random(rng),
         ];
         let output =
-            poseidon::Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash(message);
+            poseidon::Hash::<_, OrchardNullifier, ConstantLength<5>, 3, 2>::init().hash(message);
 
-        let k = 7;
-        let circuit = HashCircuit::<OrchardNullifier, 3, 2, 2> {
+        let k = 10;
+        let circuit = HashCircuit::<OrchardNullifier, 3, 2, 5> {
             message: Some(message),
             output: Some(output),
             _spec: PhantomData,
